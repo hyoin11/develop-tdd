a67e9ff16f3d9ef3021300dfcaeb975d7b0b3121
hyoin11 <kslove1111@gmail.com>
2025-04-16 18:34:10 +0900

    init project

---
+++ src/test/java/com/huey/develop_tdd/DevelopTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.develop_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
