df71000719c9bc8d5f854f51eaa8280e71e0bae0
hyoin11 <kslove1111@gmail.com>
2025-04-16 18:34:10 +0900

    init project

---
+++ src/main/java/com/huey/develop_tdd/DevelopTddApplication.java
@@ -0,0 +1,13 @@
+package com.huey.develop_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopTddApplication.class, args);
+	}
+
+}
